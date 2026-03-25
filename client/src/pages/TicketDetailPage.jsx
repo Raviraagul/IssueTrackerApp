2f7c705b1a7a11c7ad91e1299f75f75c4e3610f2
RaviRaagul <raviraagul23@gmail.com>
2026-03-25 20:48:00 +0530

    chore: removed Fixed Status from details page

--- client/src/pages/TicketDetailPage.jsx
+++ client/src/pages/TicketDetailPage.jsx
@@ -480,7 +480,7 @@ export default function TicketDetailPage() {
                                 value={ticket.platform} />
                             <Field label="Module" value={ticket.module} />
                             <Field label="Sub Module" value={ticket.sub_module} />
-                            <Field label="Fixed Status" value={ticket.fixed_status} />
+                            {/* <Field label="Fixed Status" value={ticket.fixed_status} /> */}
                         </div>
                     </div>
 
